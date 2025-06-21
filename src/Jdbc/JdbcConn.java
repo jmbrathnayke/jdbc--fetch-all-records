783ab16f6f7c7c39081a2da1af6c48acb49b3e94
Manujayaa <janithrathnayake01@gmail.com>
2025-06-21 20:00:14 +0530

    values adding

--- src/Jdbc/JdbcConn.java
+++ src/Jdbc/JdbcConn.java
@@ -1,39 +1,45 @@
 package Jdbc;
+
 import java.sql.*;
 
 public class JdbcConn {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
+        int sid = 101;
+        String sname = "manu";
+        int marks = 50;
+
         String url = "jdbc:postgresql://localhost:5432/demo";
         String uname = "postgres";
         String password = "113010";
-        String sql = "select * from students";
 
-        // Load and register PostgreSQL driver
-       // Class.forName("org.postgresql.Driver");
+        // Load and register PostgreSQL driver (optional for JDBC 4+ but recommended)
+        Class.forName("org.postgresql.Driver");
 
         // Create connection
         Connection conn = DriverManager.getConnection(url, uname, password);
-        System.out.println("connection established");
-        //create statement
+        System.out.println("Connection established");
+
+        // Create statement
         Statement st = conn.createStatement();
 
-        //execute statement
-        ResultSet rs =  st.executeQuery(sql);
-        // rs.next();
-        // String name =rs.getString("sname");
-        // System.out.println("Name of a student is " + name);
+        // Insert data
+        String insertSQL = "INSERT INTO students (sid, sname, marks) VALUES (" + sid + ", '" + sname + "', " + marks + ")";
+        st.executeUpdate(insertSQL); // use executeUpdate for insert/update/delete
 
-        //fetching all records
-        while(rs.next()){
-            System.out.print(rs.getInt(1) + "-");
-            System.out.print(rs.getString(2) + "-");
-            System.out.println(rs.getInt(3));
+        // Fetch data
+        String fetchSQL = "SELECT * FROM students";
+        ResultSet rs = st.executeQuery(fetchSQL);
+
+        // Display records
+        while (rs.next()) {
+            System.out.print(rs.getInt("sid") + " - ");
+            System.out.print(rs.getString("sname") + " - ");
+            System.out.println(rs.getInt("marks"));
         }
 
         // Close connection
         conn.close();
-
-        System.out.println("connection closed");
+        System.out.println("Connection closed");
     }
 }
